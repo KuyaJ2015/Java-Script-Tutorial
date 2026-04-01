fbd442d05bb9a25c12e403190528e683a8c6d352
KuyaJ2015 <iamjames.valmeo@gmail.com>
2026-04-01 20:02:14 +0800

    Add JSTutorial8Switch files

---
+++ PROJECTS/JSTutorial8Switch/exercise.js
@@ -0,0 +1,32 @@
+// TODO 
+/* Create program that outputs the equivalent day of a number variable */
+
+let day = 3;
+
+switch (day) {
+    case 1:
+        alert(`Monday`);
+        break;
+    case 2:
+        alert(`Tuesday`);
+        break;
+    case 3:
+        alert(`Wenesday`);
+        break;
+    case 4:
+        alert(`Thursday`);
+        break;
+    case 5:
+        alert(`Friday`);
+        break;
+    case 6:
+        alert(`Saturday`);
+        break
+    case 7:
+        alert(`Sunday`);
+        break;
+    default:
+        alert(`Invalid number chose betwwen 1 - 7`);
+
+
+}
